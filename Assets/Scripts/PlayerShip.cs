15c0a1b727f39e777833c2530c033990507f8a2c
Son Nguyen <sonnguyen9800@gmail.com>
2020-10-08 19:01:27 +0700

    Create boundaries for ship movement

--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -6,12 +6,13 @@ using UnityEngine;
 
 public class PlayerShip : MonoBehaviour
 {
-
     private Rigidbody2D rb;
     [SerializeField]
     [Range(10,100)]
     private float movespeed;
     private Vector3 mousePosition;
+
+    private float minX = (float)-3.5, maxX = (float)3.5, minY = (float)-6.0, maxY= (float)5.0;
 
     // Start is called before the first frame update
     void Start()
@@ -27,10 +28,25 @@ public class PlayerShip : MonoBehaviour
         mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-        
+        if (mousePosition.x < minX)
+        {
+            mousePosition.x = minX;
+        }
+        if (mousePosition.x > maxX)
+        {
+            mousePosition.x = maxX;
+        }
+
+        if (mousePosition.y < minY)
+        {
+            mousePosition.y = minY;
+        }
+        if (mousePosition.y > maxY)
+        {
+            mousePosition.y = maxY;
+        }
         transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+
         //rb.velocity = transform.forward*Time.deltaTime*
-       
-            
     }
 }
