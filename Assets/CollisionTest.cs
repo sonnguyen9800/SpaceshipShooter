ea5c394a77d0e7b0ff29166e080c11b9b9bb854e
Son Nguyen <sonnguyen9800@gmail.com>
2020-10-08 18:29:30 +0700

    Create movement for Spaceship

---
+++ Assets/CollisionTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTest : MonoBehaviour
+{
+   
+    private void OnTrigerEnter2D(Collider2D collider2D)
+    {
+        Debug.Log("Object Hit");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Object has been created");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   
+}
---
+++ Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerShip : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    [SerializeField]
+    [Range(10,100)]
+    private float movespeed;
+    private Vector3 mousePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        movespeed = 12;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveSpeed = movespeed / 100;
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        
+        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        //rb.velocity = transform.forward*Time.deltaTime*
+       
+            
+    }
+}
