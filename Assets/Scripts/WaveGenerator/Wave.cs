816e6dceb5ad08df2278a1cb960193ecd67ebfff
Son Nguyen <sonnguyen9800@gmail.com>
2020-10-14 18:44:52 +0700

    Clear the Wave and Location attached to it after the wave is cleared

--- Assets/Scripts/WaveGenerator/Wave.cs
+++ Assets/Scripts/WaveGenerator/Wave.cs
@@ -6,37 +6,48 @@ using UnityEngine;
 public class Wave : MonoBehaviour
 {
     private string LOG_TAG = typeof(Wave).Name;
-    [SerializeField] private float baseCooldown = 5.0f;
-    private float cooldown = 0.1f;
+    [SerializeField] private float spawningCooldown; 
 
-    // Default number of enemy = 5
     [SerializeField] private int numberEnemy = 5;
     [SerializeField] private List<Transform> moveLocations;
-    [SerializeField] private Enemy enemy1;
+    [SerializeField] private Enemy enemyModel;
+
+    private List<Enemy> enemyList = new List<Enemy>();
     void Awake(){
-     
     }
 
     private IEnumerator spawningEnemy(){
         for (int i = 0 ; i < this.numberEnemy; i++ ){
-            Debug.Log(LOG_TAG + " Spawning enemy number: " + i);
-            Enemy enemy = Instantiate(enemy1, this.transform.position, this.transform.rotation);
+            Enemy enemy = Instantiate(enemyModel, this.transform.position, this.transform.rotation);
+            this.enemyList.Add(enemy);
             foreach(var transform in this.moveLocations){
                 enemy.movePaths.Add(transform);
             }
-            yield return new WaitForSeconds(1.2f);
+            yield return new WaitForSeconds(spawningCooldown);
         }
 
     }
     void Start()
     {
-        
         StartCoroutine(spawningEnemy());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int aliveEnemy = 0;
+        foreach(Enemy enemy in this.enemyList){
+            if (enemy != null){
+                aliveEnemy ++;
+            }
+        }
+        if (aliveEnemy == 0){
+            Debug.Log("The wave has been cleared");
+            foreach(Transform transform in this.moveLocations){
+                GameObject location = transform.gameObject;
+                Destroy(location);
+            }
+            Destroy(gameObject);
+        }
     }
 }
