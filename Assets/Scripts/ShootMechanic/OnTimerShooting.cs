58b5b866f0544214045e6236463f0b06b63b127f
Khai Truong <kd.kidr@gmail.com>
2020-10-14 12:45:44 +0700

    add enemy shooting + reorganize folder

---
+++ Assets/Scripts/ShootMechanic/OnTimerShooting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTimerShooting : MonoBehaviour
+{
+    [SerializeField]
+    private float baseCooldown;
+    private float cooldown = 0;
+    [SerializeField]
+    private ShootComponent shootComponent;
+    private void Update()
+    {
+        // Not on cooldown
+        if (cooldown <= 0)
+        {
+            shootComponent.Shoot();
+            cooldown = baseCooldown;
+            return;
+        }
+        // On cooldown
+        cooldown -= Time.deltaTime;
+    }
+}
