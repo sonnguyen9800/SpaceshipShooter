3aa3f657ecde0955777718b9b963f9f5f49422b1
Son Nguyen <sonnguyen9800@gmail.com>
2020-10-16 00:22:08 +0700

    Add sound effect

---
+++ Assets/Scripts/Animation/SoundEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffect : MonoBehaviour
+{
+    [SerializeField] AudioSource sourceSpeaker;
+    [SerializeField] AudioClip audioClip;
+    private ParticleSystem particleSystem;
+
+    void Awake(){
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (particleSystem.time <= .1f){
+            sourceSpeaker.PlayOneShot(audioClip);
+        }
+    }
+}
