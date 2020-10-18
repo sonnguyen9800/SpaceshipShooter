30ec92d01aa199af87d4e9b496b287106d0a7fc2
Trung-RMIT-3695656 <s3695656@rmit.edu.vn>
2020-10-19 04:06:57 +0700

    update UI and Campaign Selection

---
+++ Assets/Scripts/UI/SceneChanger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void startCampaign()
+    {
+        SceneManager.LoadScene("Scene/Campaign List");
+    }
+
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
