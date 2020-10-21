255b475b077509395f2d1b682335c1d9d157af26
Son Nguyen <sonnguyen9800@gmail.com>
2020-10-21 18:34:43 +0700

    Add Enemy Type: Destroyer

---
+++ Assets/Scripts/Movement/AutoRotate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class AutoRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float speedRotate = 200f;
+    private GameObject owner; // Refer to the game object that has this scriptXS
+    void Start()
+    {
+        // Get the parent game object
+       owner =  transform.parent.gameObject;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,0,speedRotate*Time.deltaTime);
+    }
+}
