02885d70b01fe18e7ba9e0fcd34dc80de0127d61
sonnguyen9800 <sonnguyen9800@gmail.com>
2020-10-26 13:29:59 +0700

    AudioSource

---
+++ Assets/Scripts/Cutscene/AudioInstance.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class AudioInstance : MonoBehaviour
+{
+     private static AudioInstance instance = null;
+    public static AudioInstance Instance {
+        get { return instance; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+     void Awake() {
+     if (instance != null && instance != this) {
+         Destroy(this.gameObject);
+         return;
+     } else {
+         instance = this;
+     }
+     DontDestroyOnLoad(this.gameObject);
+    }
+}
