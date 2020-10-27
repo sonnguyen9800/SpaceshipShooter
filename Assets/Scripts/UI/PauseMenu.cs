9254b0e62f2af55d29cbb4d997ff2de3f1030e69
sonnguyen9800 <sonnguyen9800@gmail.com>
2020-10-27 18:02:34 +0700

    Add pause feature

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool PauseGame  = false;
+
+    public GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Pause game status:" + this.PauseGame);
+        this.PauseGame = false;
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if (PauseGame){
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    private void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        PauseGame = true;
+    }
+
+    private void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        PauseGame = false;
+    }
+
+}
